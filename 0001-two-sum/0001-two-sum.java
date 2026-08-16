198f6e4bf95da49d4881c565c2c92117fa2d8928
developerwordz <79623137+developerwordz@users.noreply.github.com>
2026-08-16 18:38:03 +0530

    Time: 13 ms (45.89%), Space: 46.8 MB (85.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,26 +1,26 @@
 import java.util.Arrays;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    int[][] A = new int[nums.length][2];
-    for(int i=0;i<nums.length;i++){
-        A[i][0] = nums[i];
-        A[i][1] = i;
-    }
-    Arrays.sort(A,Comparator.comparingInt(a-> a[0]));
-    int i=0,j=nums.length-1;
-    while (i<j){
-        int curr = A[i][0] + A[j][0];
-        if(curr>target){
-            j--;
+        int A[][] = new int[nums.length][2];
+        for(int i=0;i<nums.length;i++){
+            A[i][0] = nums[i];
+            A[i][1] = i;
         }
-        else if(curr<target){
-            i++;
+        Arrays.sort(A,Comparator.comparingInt(a->a[0]));
+        int i=0,j=nums.length-1;
+        while(i<j){
+            int current = A[i][0] + A[j][0];
+            if(current>target){
+                j--;
+            }
+            else if(current<target){
+                i++;
+            }
+            else{
+                return new int[]{A[i][1],A[j][1]};
+
+            }
         }
-        else{
-            return new int[]{A[i][1], A[j][1]};
-        }
-    }
-    return new int[0];
-    
+        return new int[0];
     }
 }
