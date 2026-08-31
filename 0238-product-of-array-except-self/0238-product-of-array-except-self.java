165a8a48a8c2ff0c0a1aa4227724f74024f3560e
developerwordz <79623137+developerwordz@users.noreply.github.com>
2026-08-31 21:22:35 +0530

    Time: 4 ms (6.13%), Space: 72.4 MB (6.57%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int res[] = new int[nums.length];
+        Arrays.fill(res,1);
+        int prefix=1;
+        int postfix=1;
+        for(int i=1;i<nums.length;i++){
+            res[i] = prefix*nums[i-1];
+            prefix *= nums[i-1];
+        }
+        for(int i=(nums.length-2);i>=0;i--){
+            res[i] *= (postfix*nums[i+1]);
+            postfix*=nums[i+1];
+        }
+        return res;
+    }
+}
