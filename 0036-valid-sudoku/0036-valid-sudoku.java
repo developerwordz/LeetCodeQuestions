5191e739c08153ae5f24607d07676e1515a442b8
developerwordz <79623137+developerwordz@users.noreply.github.com>
2026-09-02 23:09:45 +0530

    Time: 9 ms (21.47%), Space: 46.7 MB (30.26%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,42 +1,38 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-         HashMap<Integer,HashSet<Integer>> rows = new HashMap<>();
-         HashMap<Integer,HashSet<Integer>> columns = new HashMap<>();
-         HashMap<String,HashSet<Integer>> squares = new HashMap<>();
+        //  HashMap<Integer,HashSet<Integer>> rows = new HashMap<>();
+        //  HashMap<Integer,HashSet<Integer>> columns = new HashMap<>();
+        //  HashMap<String,HashSet<Integer>> squares = new HashMap<>();
+        Map<Integer, Set<Character>> rows = new HashMap<>();
+        Map<Integer, Set<Character>> columns = new HashMap<>();
+        Map<String, Set<Character>> squares = new HashMap<>();
+        //USING computeIfAbsent() instead
+        //  for(int i=0;i<9;i++){
+        //     rows.put(i,new HashSet<>());
+        //     columns.put(i,new HashSet<>());
+        //     for(int j=0;j<9;j++){
+        //     String coord = String.format("%d,%d",i/3,j/3);
+        //     squares.put(coord,new HashSet<>());
+        //     }
             
-         for(int i=0;i<9;i++){
-            rows.put(i,new HashSet<>());
-            columns.put(i,new HashSet<>());
-            for(int j=0;j<9;j++){
-            String coord = String.format("%d,%d",i/3,j/3);
-            squares.put(coord,new HashSet<>());
-            }
-            
-         }
+        //  }
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
                 if(board[i][j]=='.') continue;
-                int val = Character.getNumericValue(board[i][j]);
-                if(rows.getOrDefault(i,new HashSet()).contains(val)){
+                 String squareKey = (i / 3) + "," + (j / 3);
+                char val = board[i][j];
+                if(rows.computeIfAbsent(i,key->new HashSet<>()).contains(val)
+                ||columns.computeIfAbsent(j,key->new HashSet<>()).contains(val)
+                ||squares.computeIfAbsent(squareKey,key->new HashSet<>()).contains(val)
+                ){
                     return false;
                 }
                 else{
                     rows.get(i).add(val);
-                }
-                if(columns.getOrDefault(j,new HashSet()).contains(val)){
-                    return false;
-                }
-                else{
                     columns.get(j).add(val);
+                    squares.get(squareKey).add(val);
                 }
-                int x = i/3;int y = j/3;
-                String coord = String.format("%d,%d",x,y); 
-                if(squares.getOrDefault(coord,new HashSet()).contains(val)){
-                    return false;
-                }
-                else{
-                    squares.get(coord).add(val);
-                }
+                
             }
         }
         return true;
